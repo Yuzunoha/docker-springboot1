f5d42c7b1212de0d25cb826e989c37b021846420
Yuzunoha <studysteadystandby@gmail.com>
2023-02-18 12:38:25 +0900

    お勉強

--- app/demo/src/main/java/com/example/demo/controller/HelloController.java
+++ app/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.Arrays;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +11,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/index")
     public String index() {
-        System.out.println("ログは出るかな");
-        return "Dockerからこんにちは。";
+        // 入力文字列１つ
+        String in = "-1 10 3 101 555555 4";
+
+        // 文字列を配列に分解する
+        String[] a = in.split(" ");
+
+        // int型の入れ物を同じサイズで宣言する
+        int[] nums = new int[a.length];
+
+        // すべてパースして詰める
+        for (int i = 0; i < a.length; i++) {
+            nums[i] = Integer.parseInt(a[i]);
+        }
+
+        // 返却用文字列を作って返却する
+        StringBuilder sb = new StringBuilder();
+        for (var e : nums) {
+            System.out.println(e);
+            sb.append(e + " ");
+        }
+        return sb.toString();
     }
 }
